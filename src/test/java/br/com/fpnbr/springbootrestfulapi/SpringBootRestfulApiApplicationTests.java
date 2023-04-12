4cf251c424bffd7ed2483fe96bc3c324069b737a
FPNBR <felipepny@gmail.com>
2023-04-12 18:30:44 -0300

    Base do projeto criada

---
+++ src/test/java/br/com/fpnbr/springbootrestfulapi/SpringBootRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fpnbr.springbootrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
