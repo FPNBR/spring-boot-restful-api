d14b0cfb1e80008fff94fc11e6564f72a1ae00c5
FPNBR <felipepny@gmail.com>
2023-04-19 15:55:48 -0300

    Passando parâmetros para o serviço RESTfull

--- src/main/java/br/com/fpnbr/springbootrestfulapi/controllers/IndexController.java
+++ src/main/java/br/com/fpnbr/springbootrestfulapi/controllers/IndexController.java
@@ -4,13 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
     @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init() {
-        return new ResponseEntity("Olá Usuário", HttpStatus.OK);
+    public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome não informado") String nome, @RequestParam("salario") Long salario) {
+        return new ResponseEntity("Olá " + nome + " salário: " + salario, HttpStatus.OK);
     }
 }
